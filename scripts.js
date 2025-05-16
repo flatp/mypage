2eac668b9215292cae4098ce18d3deaa20d79958
flatp <flat.pp057@icloud.com>
2025-05-16 20:07:02 +0900

    update title

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 console.log("ようこそ！");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll('.project-card');
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('appear');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, {
+    threshold: 0.8
+  });
+
+  cards.forEach(card => {
+    observer.observe(card);
+  });
+});
