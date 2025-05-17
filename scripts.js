7bab08d196eed96dfd4c52fc92b2c06e0d5f2cf8
flatp <flat.pp057@icloud.com>
2025-05-17 09:22:50 +0900

    modify graphic works

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ console.log("ようこそ！");
 
 document.addEventListener("DOMContentLoaded", () => {
   const cards = document.querySelectorAll('.project-card');
+  const cards2 = document.querySelectorAll('.scroll-container');
 
   const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
@@ -11,10 +12,22 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   }, {
-    threshold: 0.8
+    threshold: 0.5
   });
 
   cards.forEach(card => {
     observer.observe(card);
   });
+  cards2.forEach(card => {
+    observer.observe(card);
+  });
+});
+
+const containers = document.querySelectorAll('.scroll-container');
+containers.forEach(container => {
+  if (container.scrollWidth <= container.clientWidth) {
+    container.style.justifyContent = 'center';
+  } else {
+    container.style.justifyContent = 'flex-start';
+  }
 });
